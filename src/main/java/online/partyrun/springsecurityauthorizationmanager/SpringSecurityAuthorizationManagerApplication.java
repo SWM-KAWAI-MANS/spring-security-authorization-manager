9f2b4dcb097c260105f000141172e3257f13aa4e
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-17 15:56:35 +0900

    feat : initial setting #PAR-83

---
+++ src/main/java/online/partyrun/springsecurityauthorizationmanager/SpringSecurityAuthorizationManagerApplication.java
@@ -0,0 +1,13 @@
+package online.partyrun.springsecurityauthorizationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthorizationManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/partyrun/springsecurityauthorizationmanager/SpringSecurityAuthorizationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package online.partyrun.springsecurityauthorizationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

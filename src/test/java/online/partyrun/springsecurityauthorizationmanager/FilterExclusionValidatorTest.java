8881b7b66d6f8addc2d1fc135a752df9a05e7868
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-18 11:11:02 +0900

    chore : test 댑스 줄임 #PAR-83

--- src/test/java/online/partyrun/springsecurityauthorizationmanager/FilterExclusionValidatorTest.java
+++ src/test/java/online/partyrun/springsecurityauthorizationmanager/FilterExclusionValidatorTest.java
@@ -1,8 +1,8 @@
 package online.partyrun.springsecurityauthorizationmanager;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("FilterExclusionValidator 에서")
 class FilterExclusionValidatorTest {
@@ -10,64 +10,60 @@ class FilterExclusionValidatorTest {
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    class 환경변수가_정상적으로_설정된_경우 {
+    class 주어진_값들이_슬래시가_붙어있을_때 {
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-        class 주어진_값들이_슬래시가_붙어있을_때 {
-            @Nested
-            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-            class 환경변수_값이_비어있을_때 {
+        class 환경변수_값이_비어있을_때 {
+            final FilterExclusionValidator exclusionValidator =
+                    new FilterExclusionValidator(new String[0]);
+
+            @Test
+            @DisplayName("아무 값을 넣어도 false를 반환한다.")
+            void doFalse() {
+                assertThat(exclusionValidator.validate(requestPath)).isFalse();
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 환경변수_값이_하나일_때 {
+            @Test
+            @DisplayName("path가 포함되면 true를 반환한다.")
+            void doTrue() {
                 final FilterExclusionValidator exclusionValidator =
-                        new FilterExclusionValidator(new String[0]);
-
-                @Test
-                @DisplayName("아무 값을 넣어도 false를 반환한다.")
-                void doFalse() {
-                    assertThat(exclusionValidator.validate(requestPath)).isFalse();
-                }
+                        new FilterExclusionValidator(new String[] {"/hello"});
+                assertThat(exclusionValidator.validate(requestPath)).isTrue();
             }
 
-            @Nested
-            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-            class 환경변수_값이_하나일_때 {
-                @Test
-                @DisplayName("path가 포함되면 true를 반환한다.")
-                void doTrue() {
-                    final FilterExclusionValidator exclusionValidator =
-                            new FilterExclusionValidator(new String[] {"/hello"});
-                    assertThat(exclusionValidator.validate(requestPath)).isTrue();
-                }
+            @Test
+            @DisplayName("path가 포함되지않으면 false를 반환한다.")
+            void doFalse() {
+                final FilterExclusionValidator exclusionValidator =
+                        new FilterExclusionValidator(new String[] {"/ttttt"});
+                assertThat(exclusionValidator.validate(requestPath)).isFalse();
+            }
+        }
 
-                @Test
-                @DisplayName("path가 포함되지않으면 false를 반환한다.")
-                void doFalse() {
-                    final FilterExclusionValidator exclusionValidator =
-                            new FilterExclusionValidator(new String[] {"/ttttt"});
-                    assertThat(exclusionValidator.validate(requestPath)).isFalse();
-                }
+        @Nested
+        @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+        class 환경변수_값이_여러개일_때 {
+            @Test
+            @DisplayName("하나의 값만 들어가도 true를 반환한다.")
+            void doTrue() {
+                final FilterExclusionValidator exclusionValidator =
+                        new FilterExclusionValidator(
+                                new String[] {"/asdf", "/qwerweq", "/hello"});
+
+                assertThat(exclusionValidator.validate(requestPath)).isTrue();
             }
 
-            @Nested
-            @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-            class 환경변수_값이_여러개일_때 {
-                @Test
-                @DisplayName("하나의 값만 들어가도 true를 반환한다.")
-                void doTrue() {
-                    final FilterExclusionValidator exclusionValidator =
-                            new FilterExclusionValidator(
-                                    new String[] {"/asdf", "/qwerweq", "/hello"});
-
-                    assertThat(exclusionValidator.validate(requestPath)).isTrue();
-                }
-
-                @Test
-                @DisplayName("하나의 값도 없으면 false를 반환한다.")
-                void doFalse() {
-                    final FilterExclusionValidator exclusionValidator =
-                            new FilterExclusionValidator(
-                                    new String[] {"/asdf", "/qwerweq", "/ttttt"});
-                    assertThat(exclusionValidator.validate(requestPath)).isFalse();
-                }
+            @Test
+            @DisplayName("하나의 값도 없으면 false를 반환한다.")
+            void doFalse() {
+                final FilterExclusionValidator exclusionValidator =
+                        new FilterExclusionValidator(
+                                new String[] {"/asdf", "/qwerweq", "/ttttt"});
+                assertThat(exclusionValidator.validate(requestPath)).isFalse();
             }
         }
     }
